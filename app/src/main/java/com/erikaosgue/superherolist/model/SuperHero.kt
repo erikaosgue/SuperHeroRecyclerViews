03cb12935f7a78fab8775a4a784559f9fab4ba39
erikaosgue <1476@holbertonschool.com>
2021-03-23 23:22:57 -0500

    Remove all files from current project

--- app/src/main/java/com/erikaosgue/superherolist/model/SuperHero.kt
+++
@@ -1,6 +0,0 @@
-package com.erikaosgue.superherolist.model
-
-data class SuperHero(val superHeroName: String,
-                     val publisher: String,
-                     val realName: String,
-                     val image: String)
